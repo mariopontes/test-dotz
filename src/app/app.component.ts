d0252b43aaba29d415119a76972ae01a2f9d9df6
Mário <mario.canys@hotmail.com>
2021-05-27 14:52:10 -0300

    --

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,15 @@ import { AuthService } from './core/services/auth.service';
 export class AppComponent {
   title = 'test-dotz';
   isLogin: boolean = false;
+  fixHeight = '';
 
   constructor(
     private router: Router,
     private authService: AuthService
   ) {
     this.router.events.subscribe(e => {
-      (location.pathname === '/auth/login' || location.pathname === '/auth/sign-up') ? this.isLogin = true : this.isLogin = false
+      (location.pathname === '/auth/login' || location.pathname === '/auth/sign-up') ? this.isLogin = true : this.isLogin = false;
+      location.pathname === '/order' ? this.fixHeight = 'fix-height' : this.fixHeight = ' ';
     });
 
     if (localStorage.getItem('currentUser')) {
